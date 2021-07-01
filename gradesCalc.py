3e9932b58a6489b01a9112e95d738acd67da9d79
Murad <muradek@campus.technion.ac.il>
2021-07-01 17:36:42 +0300

    debugged and added part2

--- gradesCalc.py
+++ gradesCalc.py
@@ -1,47 +1,63 @@
 #### PART 1 ####
-    
-# check if a line in the input file is legal
-# if its legal, insert to the dictionary 
 def insertLineToDict(line: str, students_dict: dict): 
     # get the values from the line
     line_list = line.split(",")
-    id_num = int(line_list[0])
-    semester = int(line_list[2])
-    hw_avg = int(line_list[3])
 
-    temp_name = line_list[1]
-    name_list = temp_name.split(' ')
-    no_spaces_name_list = []
-    for item in name_list:
-        if item != "":
-            no_spaces_name_list.append(item)
+    # if value is illegal, return
 
-    name = " ".join(no_spaces_name_list)
-
-    # check if values are legal
-    # if not, return
-    if (str(id_num)[0] == '0' or len(str(id_num)) != 8):
+    # checking the student's ID
+    str_with_tabs_and_spaces = line_list[0]
+    # removing spaces from the ID
+    list_with_tabs_no_spaces = str_with_tabs_and_spaces.split(" ")
+    str_with_tabs_no_spaces = "".join(list_with_tabs_no_spaces)
+    # removing tabs from the ID
+    list_no_tabs_no_spaces = str_with_tabs_no_spaces.split("\t")
+    id_str = "".join(list_no_tabs_no_spaces)
+    id_num = int(id_str)
+    if (id_str[0] == '0' or len(str(id_num)) != 8):
         return
+
+    # checking the student's name
+    temp_name = line_list[1]
+
+    # remove tabs from the string
+    name_list_no_tabs = temp_name.split("\t")
+    name_str_no_tabs = " ".join(name_list_no_tabs)
+
+    #remove unnecessary spaces
+    name_list_with_null = name_str_no_tabs.split(' ')
+    name_list_no_null = []
+    
+    for item in name_list_with_null:
+        if item != "":
+            name_list_no_null.append(item)
+
+    name = " ".join(name_list_no_null)
 
     for chr in name:
         if (not( 
         (ord(chr) == ord(' ')) #chr is space
         or (ord(chr) <= ord('z') and ord(chr) >= ord('a')) #chr is uppercase
-        or (ord(chr) <= ord('Z') and ord(chr) >= ord('A'))) #chr is lowercase
-        ): 
+        or (ord(chr) <= ord('Z') and ord(chr) >= ord('A')) #chr is lowercase 
+        )):
             return
-    
+
+    # checking the student's semester
+    semester = int(line_list[2])
     if semester < 1:
         return
 
+    # checking the student's HW AVG
+    hw_avg = int(line_list[3])
     if (hw_avg <= 50 or hw_avg > 100):
         return
+
 
     # at this point of the function, line is legal
     # insert the student to the dictionary
     students_dict[id_num] = [name , semester , hw_avg]
+    return
   
-
 # final_grade: Calculates the final grade for each student, and writes the output (while eliminating illegal
 # rows from the input file) into the file in `output_path`. Returns the average of the grades.
 #   input_path: Path to the file that contains the input
@@ -74,6 +90,34 @@ def final_grade(input_path: str, output_path: str) -> int:
         output_file.write(line_to_print)
 
     if grades_count == 0:
-        return
+        return 0
     
     return (grades_sum//grades_count)
+    
+
+#### PART 2 ####
+# check_strings: Checks if `s1` can be constructed from `s2`'s characters.
+#   s1: The string that we want to check if it can be constructed
+#   s2: The string that we want to construct s1 from
+def check_strings(s1: str, s2: str) -> bool:
+    # Create a count array and count
+    # frequencies characters in s2
+    s1=s1.lower()
+    s2=s2.lower()
+    
+    if len(s1) == 0 and len(s2) == 0:
+        return True
+    if len(s1)>len(s2):
+        return False
+    count = {s2[i] : 0 for i in range(len(s2))}
+    
+    for i in range(len(s2)):
+        count[s2[i]] += 1
+    
+    # Now traverse through str1 to check
+    # if every character has enough counts
+    for i in range(len(s1)):
+        if (count.get(s1[i]) == None or count[s1[i]] == 0):
+            return False
+        count[s1[i]] -= 1
+    return True
