7db8f9cc7ee3f198edaf87fe9a4e398641409163
Murad <muradek@campus.technion.ac.il>
2021-07-01 14:22:35 +0300

    implemented final_grade() function

--- gradesCalc.py
+++ gradesCalc.py
@@ -42,25 +42,38 @@ def insertLineToDict(line: str, students_dict: dict):
     students_dict[id_num] = [name , semester , hw_avg]
   
 
-
-
-
 # final_grade: Calculates the final grade for each student, and writes the output (while eliminating illegal
 # rows from the input file) into the file in `output_path`. Returns the average of the grades.
 #   input_path: Path to the file that contains the input
 #   output_path: Path to the file that will contain the output
 def final_grade(input_path: str, output_path: str) -> int:
+    input_file = open(input_path, "r")
+    output_file = open(output_path, "w")
+    students_dictionary = {}
+    for line in input_file:
+        insertLineToDict(line, students_dictionary)
     
+    ids_list = []
+    for id_num in students_dictionary:
+        ids_list.append(id_num)
 
-
+    ids_list.sort()
     
-    raise NotImplementedError
+    grades_sum = 0
+    grades_count = 0
 
+    for id_num in ids_list:
+        # student's grade is the avg of his hw grade
+        # and the last two digits of his ID num
+        hw_avg = students_dictionary[id_num][2]
+        student_final_grade = (hw_avg + id_num%100)//2
+        grades_sum += student_final_grade
+        grades_count += 1
 
-#### PART 2 ####
-# check_strings: Checks if `s1` can be constructed from `s2`'s characters.
-#   s1: The string that we want to check if it can be constructed
-#   s2: The string that we want to construct s1 from
-def check_strings(s1: str, s2: str) -> bool:
-    # TODO: implement here
-    raise NotImplementedError
+        line_to_print = str(id_num) + ", " + str(hw_avg) + ", " + str(student_final_grade) + '\n'
+        output_file.write(line_to_print)
+
+    if grades_count == 0:
+        return
+    
+    return (grades_sum//grades_count)
